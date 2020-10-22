660a99998fe9de7453c51b1cf4ab75c9de4bc78d
Abhishek Punj <72729093+AbhishekPunj0812@users.noreply.github.com>
2020-10-22 16:46:00 +0530

    Code: Check if Permission for the role

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -1,43 +1,35 @@
-let d;
-let permissions =
-{
+let permission = {
     'getUsers': {
     all: ['head-trainer'],
     read : ['trainee', 'trainer'],
     write : ['trainer'],
     Delete: [],
     }
-    }
-    const {getUsers}= permissions;
-    //console.log(getUsers);
-    let f;
-    function hasPermission(moduleName,role,permissionType)
-    {
-        const {all,read,write,Delete}=moduleName;
-         f = all.includes(role)
-
-        if(f==true)
-        {
-            return true
-        }
-        else
-        {
-            if(permissionType=="read")
-            {
-                f=read.includes(role)
-                return f;
-            }
-            else if(permissionType=="write"){
-                f=write.includes(role);
-                return f;
-            }
-            else if(permissionType=="Delete"){
-                f=Delete.includes(role)
-                return f;
-            }
-
-        }
 
     }
-    d = hasPermission(getUsers,"trainer","Delete");
-    console.log(d);
+    
+
+function hasPermission(moduleName,role,permissionType){
+    for(const [key,value] of Object.entries(permission)){
+        if(key == moduleName){
+            if(value.all.includes(role)){
+                return true;
+            }else{
+                for(const[key1,value1] of Object.entries(value)){
+                    if(key1 === permissionType){
+                        if(value1.includes(role)){
+                            return true;
+                        }
+                        return false;
+                    }else{
+                        continue;
+                    }
+                }
+                }
+        }else{
+            continue;
+        }
+    }
+}
+const result =hasPermission("getUsers","head-trainer","delete");
+    console.log(result);
