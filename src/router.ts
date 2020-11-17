91fe5cc9074d49e226eecc434ed62f44142c5ff4
Abhishek Punj <72729093+AbhishekPunj0812@users.noreply.github.com>
2020-11-17 15:28:50 +0530

    Code Fixed

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ import { userRouter }  from './controllers/user';
 
 const mainRouter = Router();
 
-// mainRouter.use('/trainee', TraineeRouter);
+mainRouter.use('/trainee', TraineeRouter);
 mainRouter.use('/user', userRouter);
 
 export default mainRouter;
