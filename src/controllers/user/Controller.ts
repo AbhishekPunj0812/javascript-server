5b3b1d15544260d0dba98d78673ebe36e96c9267
Abhishek Punj <72729093+AbhishekPunj0812@users.noreply.github.com>
2020-12-04 13:09:30 +0530

    Update Controller.ts

--- src/controllers/user/Controller.ts
+++ src/controllers/user/Controller.ts
@@ -1,206 +1,183 @@
-import { NextFunction, Request, Response } from 'express';
-import  * as jwt from 'jsonwebtoken';
-import { userModel } from '../../repositories/user/UserModel';
-import config from '../../config/configuration';
-import IRequest from '../../libs/routes/IRequest';
-import * as bcrypt from 'bcrypt';
+import { Request, Response, NextFunction } from 'express';
 import UserRepository from '../../repositories/user/UserRepository';
-class UserController {
-    instance: UserController;
+import IRequest from '../../libs/routes/IRequest';
+import { error } from 'console';
+
+class TraineeController {
+    instance: TraineeController;
     static instance: any;
-    static getInstance() {
-        if (UserController.instance) {
-          return UserController.instance;
-        }
-      UserController.instance = new UserController();
-        return UserController.instance;
+  static getInstance() {
+      if (TraineeController.instance) {
+        return TraineeController.instance;
+      }
+    TraineeController.instance = new TraineeController();
+      return TraineeController.instance;
     }
 
-    public async get(req: IRequest, res: Response, next: NextFunction) {
-      try {
-        console.log('Inside get method of User Controller');
-        res.send({
-          message: 'User fetched successfully',
-          data: [
-            {
-              name: 'User1'
-            },
-            {
-                name: 'User2'
-            }
-          ]
-        });
-      }
-      catch (err) {
-        console.log('Inside err', err);
-        next({
-          error: 'Error Occured in fetching user',
-          code: 500,
-          message: err
-        });
-      }
-    }
     public async getAll(req: IRequest, res: Response, next: NextFunction) {
 
       let skip: number;
       let limit: number;
       let sort: boolean;
+      let search: string = '';
 
-      limit = ('limit' in req.query) ? Number(req.query.limit) : 10;
+        limit = ('limit' in req.query) ? Number(req.query.limit) : 10;
 
-      skip = ('skip' in req.query) ? Number(req.query.skip) : 0;
+        skip = ('skip' in req.query) ? Number(req.query.skip) : 0;
 
-      sort = ('sort' in req.query) ? Boolean(req.query.sort) : false;
+        sort = ('sort' in req.query) ? Boolean(req.query.sort) : false;
 
-      try {
-        const user = new UserRepository();
-        const data = await user.getall(skip, limit, sort);
-            res.status(200).send({
-                message: 'Trainees fetched successfully',
-                trainee: {
-                'count': data[1],
-                'data':   data[0]
-                }
-            });
-
+      if ('search' in req.query) {
+          search = req.query.search;
       }
-      catch (err) {
-          next({
-              error : 'Unable to fetch Trainees',
-              code : 404,
-              message : err
-          });
-      }
-    }
-    public async create(req: IRequest, res: Response, next: NextFunction) {
-      const { id, email, name, role, password } = req.body;
-      const creator = req.user._id;
-      try {
-        const user = new UserRepository();
-        await user.create({ id, email, name, role, password }, creator);
-              res.send({
-                  message: 'User Created Successfully!',
-                  data: {
-                      'name': name,
-                      'email': email,
-                      'role': role,
-                      'password': password
-                  },
-                  code: 200
-              });
-      }
-          catch (err) {
-            next({
-              error: 'Error Occured in creating user',
-              code: 500,
-              message: err
-            });
-          }
 
-    }
+      const query: any = {
+          deletedAt: { $exists: false },
+          updatedAt: { $exists: false },
+          role: 'trainee',
+          $or: [
+              {
+                  name: {
+                      $regex: search,
+                      $options: 'i'
+                  }
+              },
+              {
+                  email: {
+                      $regex: search,
+                      $options: 'i'
+                  }
+              }
+          ]
+      };
 
-    public async update(req: IRequest, res: Response, next: NextFunction) {
-      const { id, dataToUpdate } = req.body;
-      const updator = req.user._id;
+      const options: any = {
+          skip: skip,
+          limit: limit
+      };
+
+      let sortQuery: any;
+
+      if (sort) {
+          sortQuery = {
+              name: 1,
+              email: 1
+          };
+      }
+      else {
+          sortQuery = {
+              createdAt: -1
+          };
+      }
+
       const user = new UserRepository();
       try {
-          await user.updateUser( id, dataToUpdate, updator);
-          res.send({
-              message: 'User Updated',
-              code: 200
-          });
-      }
-      catch (err)  {
-        next({
-          error: 'Error Occured in updating user',
-          code: 500,
-          message: err
+        const data = await user.getallTrainee(query, options, sortQuery);
+
+        res.status(200).send({
+          message: 'Trainees fetched successfully',
+          Trainees: {
+            data : {
+              count : data[1],
+              records : data[0]
+            }
+          }
         });
       }
-    }
-
-
-    public async login(req: IRequest , res: Response , next: NextFunction) {
-      try {
-        const { email, password } = req.body;
-        const result = await this.userRepository.findOne ( { 'email': email } );
-          if ( result ) {
-            if ( bcrypt.compareSync(req.body.password, result. password) ) {
-                console.log('result is', result.password);
-                const token = jwt.sign({
-                    id: result._id,
-                    email: result.email
-                }, config.SECRET_KEY);
-                console.log( token );
-                res. send( {
-                    data: { ...result, token },
-                    message: 'Login Permitted',
-                    status: 200
-                });
-            }
-            else {
-              res.send ( {
-                  message: "password doesn't match",
-                  status: 400
+          catch (err) {
+              next({
+                  message: 'Trainee Not Found',
+                  status: 404,
+                  error: err
               });
-            }
-          } else {
-                res.send ( {
-                  message: ' Email is not registered ',
-                  status: 404
-                });
-            }
-
+          }
       }
 
-          catch ( err ) {
-            next({
-              error: 'Error Occured while login',
-              code: 500,
-              message: err
-            });
-          }
-    }
+    public async create(req: IRequest, res: Response, next: NextFunction) {
+      const {  name, email, role, password } = req.body;
+      const user = new UserRepository();
+      const creator = req.user._id;
+              try {
+                await user.create({  name, email, role, password }, creator);
 
-    me (req: IRequest, res: Response, next: NextFunction) {
-        res.json(req.user);
-    }
-
-
-    public async remove(req: IRequest, res: Response, next: NextFunction) {
-            const  id  = req.params.id;
-            const remover = req.userData._id;
-            const user = new UserRepository();
-            try {
-                    await user.deleteData(id, remover);
-                    res.send({
-                    message: 'Deleted successfully',
+                res.status(200).send({
+                    message: 'Trainee Created Successfully!',
+                    data: {
+                        'name': name,
+                        'email': email,
+                        'role': role
+                    },
                     code: 200
-                    });
+                });
+              }
+              catch (err) {
+                  next({
+                      message: 'User not created',
+                      code: 404,
+                      error: err
+                  });
+              }
+      }
+      public async update(req: IRequest, res: Response, next: NextFunction) {
+        const { id, dataToUpdate } = req.body;
+        const user = new UserRepository();
+        const updator = req.user._id;
+            try {
+              const result = await user.updateUser(id, dataToUpdate, updator);
+              if (result !== undefined) {
+                res.status(200).send({
+                  message: 'Trainee Updated Successfully',
+                  data: {
+                    'id': id
+                  }
+                });
+              }
+              else {
+                res.send(404).send( {
+                  message: 'Record not found',
+                  code: 404
+                });
+              }
             }
             catch (err) {
                 next({
-                  error: 'User not found to be deleted',
-                  code: 404,
-                  message: err
+                    message: 'User Not Found for update',
+                    code: 404,
+                    error: err
                 });
             }
-    }
+        }
 
+        public async delete(req: IRequest, res: Response, next: NextFunction) {
+          const id = req.params.id;
+          const user = new UserRepository();
+              try {
+                const deletor = req.user._id;
+                const result = await user.delete(id, deletor);
+                if (result !== undefined) {
+                  res.send({
+                    message: 'Trainee Deleted Successfully',
+                    code: 200,
+                    data: result
+                });
+                }
+                else {
+                  res.send({
+                    message: 'Record does not exist in DB',
+                    code: 404
+                  });
+                }
 
-    delete(req: Request , res: Response , next: NextFunction) {
-          try {
-              console.log('User delete method of Trainee Controller');
-              res.send({
-                  message: 'User deleted successfully',
-                  data: [ { name: 'User1'}, {name: 'User2'} ]
-              });
-          }
-          catch (err) {
-                  console.log('Inside err', err);
-          }
-    }
+              }
+              catch (err) {
+                  next({
+                      message: 'User not found to be deleted',
+                      code: 404,
+                      error: err
+                  });
+              }
+        }
 
 }
 
-export default  new UserController();
+  export default  new TraineeController();
